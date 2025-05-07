708d1b7e2ec6dbd1ae27311b9d60888377bba8d3
Jinx <jinxedman21@gmail.com>
2025-05-07 15:43:22 -0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
+# MIT License
+# Copyright (c) 2025 JinxedUp
+
 # User Agents List
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.94 Safari/537.36",
